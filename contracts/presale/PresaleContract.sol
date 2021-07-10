78d7ef2ed1574f38016ddfdd8dce15d4007ff66c
kup-id <kupid@ik.me>
2021-07-10 15:15:48 +0200

    autopair

--- contracts/presale/PresaleContract.sol
+++ contracts/presale/PresaleContract.sol
@@ -8,90 +8,175 @@ import "./deps/SafeERC20.sol";
 import "./deps/IERC20.sol";
 import "./deps/SafeMath.sol";
 import "./deps/ReentrancyGuard.sol";
+import "./deps/IcupcakeFactory.sol";
+import "./deps/IcupcakeRouter02.sol";
 
 
 contract PresaleContract is Context, Ownable, ReentrancyGuard{
-    
-    using SafeMath for uint256;
-   
 
+    // TODO keep ETH or switch with WETH
+    // TODO set WETH amount to add to the pool
+    // TODO set Reward token to add to the pool
+
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    
+    // info of each holder
+    struct UserInfo public {
+         
+        uint256 startBlock, // first time holder have been send eth
+        uint256 numberOfSwap,   // number of swap
+        uint256 ethAmount,  // total eth amount swaped by holder
+        uint256 rewardPending,  // total reward pending according to calculation
+        uint256 lastSwap    // last time holder send eth
+    }
+
+    // kupcake router
+    IcupcakeRouter02 public kupcakeRouter;
+
+    // token that will be release at releaseTimeStamp
     IERC20 public rewardToken;
 
-    uint256 rewardTokenBalance;
-    uint256 busdTokenBalance;
+    // amount calculation of reward token : (eth x priceRatio) / priceRatioAgainst
     uint256 public priceRatio = 10000;
     uint256 public priceRatioAgainst = 10000;
-    mapping (address => bool) isHolder;
-    uint256 holders;
-    uint256 public totalEthSwaped;
-    uint256 public rewardTokenTotalAmount;
 
-     using SafeERC20 for IERC20;
+    // number of holders that swap
+    uint256 public holders;
 
+    // total amount of eth swaped in the pool
+    uint256 public ethTotalSwaped;
+
+    // Total amount that will be swap at releaseTimeStamp
+    uint256 public rewardTokenTotalToSwap;
+
+    // the unix timestamp since rewards could be claimed
+    uint256 public releaseTimeStamp;
+
+    // bool that show if the pool is deployed
+    bool isPoolDeployed = false;
+
+    // is msg.sender already a holder
+    mapping  (address => bool) public isHolder;
    
-    constructor(IERC20 _rewardToken) public {
-        
+    constructor(IERC20 _rewardToken, uint256 _releaseTimeStamp, address _routerAddress) public {
         rewardToken = _rewardToken;
-        
+        releaseTimeStamp = _releaseTimeStamp;
+        kupcakeRouter = IcupcakeRouter02(_routerAddress);
     }
 
+    // to add eth to the pool and calculate pending rewards
     function swapBusdToToken() public payable nonReentrant{
+        UserInfo[_msgSender()] storage user;
         uint256 amount = msg.value;
         require(amount>=0, "value must be superior to 0.");
-        totalEthSwaped = totalEthSwaped.add(amount);
+        require(block.timestamp < releaseTimeStamp, "the presale is finished")
+        ethTotalSwaped = ethTotalSwaped.add(amount);
         uint256 rewardTokenAmount = amount.mul(priceRatio).div(priceRatioAgainst);
-        require(getBalanceRewardToken()>=rewardTokenAmount, "not enough found in contract.");
-        IERC20(rewardToken).approve(address(msg.sender), rewardTokenAmount);
-        IERC20(rewardToken).safeTransfer(address(_msgSender()), rewardTokenAmount);
-        if(isHolder[msg.sender]==false){
-            isHolder[msg.sender]=true;
+        user.startBlock==0?user.startBlock = block.timestamp:user.startBlock = user.startBlock;
+        user.numberOfSwap = user.numberOfSwap.add(1);
+        user.ethAmount = user.ethAmount.add(amount);
+        user.rewardPending = user.rewardPending.add(rewardTokenAmount);
+        user.lastSwap = block.timestamp;
+         if(isHolder[_msgSender()]==false){
+            isHolder[_msgSender()]=true;
             holders.add(1);
         }
-        rewardTokenTotalAmount = rewardTokenTotalAmount.add(rewardTokenAmount);
+        rewardTokenTotalToSwap = rewardTokenTotalToSwap.add(rewardTokenAmount);
+    }
+
+    // to claims rewards since releaseTimeStamp
+    function claimRewards() public {
+        UserInfo[_msgSender()] storage user;
+        require(block.timestamp >= releaseTimeStamp, "not yet time to withdraw");
+        require(user.rewardPending>0, "nothing to withdraw");
+        require(getBalanceRewardToken()>= user.rewardPending, "not enough found in contract.");
+        if(isPoolDeployed == false){
+            address factoryAddress = kupcakeRouter.factory();
+            IcupcakeFactory Factory = IcupcakeFactory(factoryAddress);
+            Factory.createPair(address(rewardToken),kupcakeRouter.WETH());
+            uint256 wethLiquidity= 1; //TODO set Weth to the pool
+            uint256 rewardTokenLiquidity = 1 ; //TODO set amont token
+            IERC20(kupcakeRouter.WETH()).approve(address(kupcakeRouter), wethLiquidity);
+            IERC20(rewardToken).approve(address(kupcakeRouter), rewardTokenLiquidity);
+            kupcakeRouter.addLiquidityETH(
+                address(rewardToken),
+                rewardTokenLiquidity,
+                rewardTokenLiquidity,
+                owner(),
+                block.timestamp
+                )
+            isPoolDeployed = true;
+        }
+        IERC20(rewardToken).approve(address(_msgSender()), user.rewardPending);
+        IERC20(rewardToken).safeTransfer(address(_msgSender()), user.rewardPending);
+        user.rewardPending = 0;
     }
     
-      function withdraw(uint256 _amount) public onlyOwner nonReentrant{
+    // owner: to withdraw amount of eth
+    function withdraw(uint256 _amount) public onlyOwner nonReentrant{
         require(address(this).balance >= _amount, "not enough balance to withdraw ");
         payable(_msgSender()).transfer(_amount);
     }
-    
-     function withdrawRewardToken(uint256 _amount) public onlyOwner nonReentrant{
+
+    // owner: to withdraw amount of reward token
+    function withdrawRewardToken(uint256 _amount) public onlyOwner nonReentrant{
         require(_amount > 0 && getBalanceRewardToken()>= _amount, "not enough balance to withdraw.");
         IERC20(rewardToken).approve(address(this), _amount);
         rewardToken.safeTransfer(_msgSender(), _amount);
     }
     
-      function withdrawAll() public onlyOwner nonReentrant{
+    // owner: to withdraw all amount of eth
+    function withdrawAll() public onlyOwner nonReentrant{
          require(address(this).balance >= 0, "nothing to withdraw ");
         payable(_msgSender()).transfer(address(this).balance);
     }
     
-      function withdrawAllRewardToken() public onlyOwner nonReentrant{
+    // owner: to withdraw amount of reward token
+    function withdrawAllRewardToken() public onlyOwner nonReentrant{
         require(getBalanceRewardToken() >= 0, "nothing to withdraw");
         IERC20(rewardToken).approve(address(this), getBalanceRewardToken());
         IERC20(rewardToken).transfer(_msgSender(), getBalanceRewardToken());
     }
     
-     function setPriceRatio(uint256 _priceRatio, uint256 _priceRatioAgainst) public onlyOwner nonReentrant{
+    // owner: to set reward calculation
+     function setPriceRatio(uint256 _priceRatio, uint256 _priceRatioAgainst) public onlyOwner{
          priceRatio = _priceRatio;
          priceRatioAgainst = _priceRatioAgainst;
     }
     
-    
-    
+    // get data for the frontend
     function getData() public view returns( uint256[9] memory ){
-        return  [priceRatio,priceRatioAgainst, getBalanceRewardToken(), getBalance(), rewardToken.balanceOf(address(msg.sender)), address(msg.sender).balance, rewardTokenTotalAmount, totalEthSwaped, holders];
+        return  [priceRatio,priceRatioAgainst, getBalanceRewardToken(), getBalance(), rewardToken.balanceOf(address(_msgSender())), address(_msgSender()).balance, rewardTokenTotalToSwap, ethTotalSwaped, holders];
     }
     
-     function getBalanceRewardToken() public view returns(uint256) {
+    // reward token balance of the pool 
+    function getBalanceRewardToken() public view returns(uint256) {
         return rewardToken.balanceOf(address(this));
     }
     
-     function getBalance() public view returns(uint256) {
+    // eth balance of the pool 
+    function getBalance() public view returns(uint256) {
         return address(this).balance;
     }
+
+    // owner : set the reward token
+    function setRewardToken(IERC20 _rewardToken) public onlyOwner {
+        rewardToken = _rewardToken;
+    }
+
+    // owner : set release timestamp
+    function setReleaseTimeStamp(uint256 _releaseTimeStamp) public onlyOwner {
+        releaseTimeStamp = _releaseTimeStamp;
+    }
+
+    // owner: set router address
+    function setRouterAddress(address _routerAddress) public onlyOwner {
+        kupcakeRouter = IcupcakeRouter02(_routerAddress);
+
+    }
     
-    
+    // enable contract to recive ETH
     receive() external payable {}
   
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
 !!! this is not for production purpose ;)
-work with 0.6.12 compiler version
+
+work with 0.6.2 compiler version
+
+// DONE timelock for pending token to claim
+// DONE once automatic pair creation at reward timestamp 
+
+// TODO keep ETH or switch with WETH
+// TODO set WETH amount to add to the pool
+// TODO set Reward token to add to the pool
+// TODO testnet tests
